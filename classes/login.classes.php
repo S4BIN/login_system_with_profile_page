59b1178cbd3ccd3a14c3085c4cbd497eae1f291d
sabin <burlacusabin04@yahoo.com>
2023-05-17 20:38:04 +0300

    new file

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -1,10 +1,10 @@
 <?php
 
 class Login extends Dbh{
-    protected function setUser($uid, $pwd) {
+    protected function getUser($uid, $pwd) {
         $stmt = $this->connect()->prepare('SELECT users_pwd FROM users WHERE users_uid = ? OR users_email = ?;'); 
 
-        if(!$stmt->execute(array($uid, $pwd))) {
+        if(!$stmt->execute(array($usn, $usn))) {
             $stmt = null;
             header("location: ../index.php?error=stmtfailed");
             exit();
@@ -27,7 +27,7 @@ class Login extends Dbh{
         elseif($checkPwd == true) {
             $stmt = $this->connect()->prepare('SELECT * FROM users WHERE users_uid = ? OR users_email = ? AND users_pwd = ?;'); 
 
-            if(!$stmt->execute(array($uid, $uid, $pwd))) {
+            if(!$stmt->execute(array($usn, $usn, $pwdHashed[0]['user_pwd']))) {
                 $stmt = null;
                 header("location: ../index.php?error=stmtfailed");
                 exit();
