2fa633993d083f5c426ac2c1949736084298dc3b
sabin <burlacusabin04@yahoo.com>
2023-05-19 22:13:39 +0300

    modified file

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -4,30 +4,31 @@ class Login extends Dbh{
     protected function getUser($uid, $pwd) {
         $stmt = $this->connect()->prepare('SELECT users_pwd FROM users WHERE users_uid = ? OR users_email = ?;'); 
 
-        if(!$stmt->execute(array($usn, $usn))) {
+        if(!$stmt->execute(array($uid, $uid))) {
             $stmt = null;
             header("location: ../index.php?error=stmtfailed");
             exit();
         }
 
-        if($stmt->rowCount() == 0) {
-            $stmt = null;
-            header("location: ../index.php?error=usernotfound");
-            exit();
+        $loginData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if(count($loginData ) == 0) {
+                    $stmt = null;
+                    header("location: login.php?error=usernotfound");
+                    exit();
         }
+        return $profileData;
 
-        $pwdHashed = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $checkPwd = password_verify($pwd, $pwdHashed[0]["users_pwd"]);
 
-        if(checkPwd == false) {
+        if($checkPwd == false) {
             $stmt = null;
             header("location: ../index.php?error=wrongpassword");
             exit();
         }
         elseif($checkPwd == true) {
-            $stmt = $this->connect()->prepare('SELECT * FROM users WHERE users_uid = ? OR users_email = ? AND users_pwd = ?;'); 
+            $stmt = $this->connect()->prepare('SELECT * FROM users WHERE (users_uid = ? OR users_email = ?) AND users_pwd = ?;'); 
 
-            if(!$stmt->execute(array($usn, $usn, $pwdHashed[0]['user_pwd']))) {
+            if(!$stmt->execute(array($uid, $pwd, $pwdHashed[0]['user_pwd']))) {
                 $stmt = null;
                 header("location: ../index.php?error=stmtfailed");
                 exit();
@@ -48,8 +49,6 @@ class Login extends Dbh{
             $stmt = null;
     
         }
-
         $stmt = null;
     }
-
 }
