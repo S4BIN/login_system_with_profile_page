1c7e309093ec78147dba7e58beb074d57a68feaf
sabin <burlacusabin04@yahoo.com>
2023-05-22 22:34:49 +0300

    updated file

--- classes/profileinfo-contr.classes.php
+++ classes/profileinfo-contr.classes.php
@@ -10,4 +10,22 @@ class ProfileInfoContr extends ProfileInfo {
         $this->userUid = $userUid;
     }
 
+    public function defaultProfileInfo() {
+        $profileAbout = "Some about random text for profile";
+        $profileTitle = "Hi! I'm " . $this->userUid;
+        $profileText = "Welcome to my profile page! Soon I'll be able to tell you more about myself.";
+        $this->setProfileInfo($profileAbout, $profileTitle, $profileText, $this->$userId);
+    }
+
+    public function updateProfileInfo($about, $introTitle, $introText) {
+
+    }
+
+    private function emptyInputCheck($about, $introTitle, $introText) {
+        $result;
+        if(empty($about) || empty($introTitle) || empty($introText)) {
+            $result = false;
+        }
+    }
+
 }
