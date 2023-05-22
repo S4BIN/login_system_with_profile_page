a3736ffe4e7b4dcb89fa2d31bf63b9e5aafc735e
sabin <burlacusabin04@yahoo.com>
2023-05-22 22:23:06 +0300

    updated files

--- classes/profileinfo-contr.classes.php
+++ classes/profileinfo-contr.classes.php
@@ -1,5 +1,13 @@
 <?php
 
 class ProfileInfoContr extends ProfileInfo {
-    
+
+    private $userId;
+    private $userUid;
+
+    public function __constract($userId, $userUid) {
+        $this->userId = $userId;
+        $this->userUid = $userUid;
+    }
+
 }
